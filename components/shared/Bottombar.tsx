188b74fef110af9870536a2e885da80f2d716529
sevans <sevans@highdesertmilk.com>
2023-11-15 10:55:24 -0700

    home page finished

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,5 +1,30 @@
+'use client'
+import { sidebarLinks } from "@/constants"
+import Image from "next/image"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
 function Bottombar() {
-    return <h1>Bottombar</h1>
+    const pathname = usePathname()
+
+    return (
+        <section className="bottombar">
+            <div className="bottombar_container">
+            {sidebarLinks.map((link) => {
+                    const isActive = (pathname.includes(link.route) && link.route.length > 1) ||
+                    pathname === link.route
+
+                    return (                                                                    // if isActive...
+                        <Link href={link.route} key={link.label} className={`bottombar_link ${isActive && 'bg-primary-500'}`}>
+                            <Image src={link.imgURL} alt={link.label} width={24} height={24}/>              {/*regular expression, s is for words*/}
+                            <p className="text-subtle-medium text-light-1 max-sm:hidden">{link.label.split(/\s+/)[0]}</p> {/*this just gets the first word*/}
+                        </Link>
+                    )}
+                )}
+
+            </div>
+        </section>
+    )
 }
 
 export default Bottombar
